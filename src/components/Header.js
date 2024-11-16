4126f413f29928b26586872beb93098cd212987e
Anky-mohi <himalayastechies@gmail.com>
2024-11-16 05:32:00 -0800

    Your commit message

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,66 +19,81 @@ function Header() {
     setDropdownOpen((prev) => ({ ...prev, [name]: false }));
   };
 
+  const handleLinkClick = () => {
+    setMenuOpen(false); // Close menu on link click (mobile-friendly)
+  };
+
   return (
-    <>
-      <header className="header">
-        <nav className="navbar">
-          <div className="logo">
-            <Link to="/"><img src={logoImage} alt="Logo" /></Link>
-          </div>
+    <header className="header">
+      <nav className="navbar">
+        {/* Logo Section */}
+        <div className="logo">
+          <Link to="/"><img src={logoImage} alt="Logo" /></Link>
+        </div>
 
-          <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
-            <ul>
-              <li><Link to="/SellersListing">Marketplace</Link></li>
-              <li><Link to="/buyers">For Sellers</Link></li>
-              
-              {/* Hoverable Dropdown for Pricing */}
-              <li 
-                className="dropdown" 
-                onMouseEnter={() => handleMouseEnter('pricing')} 
-                onMouseLeave={() => handleMouseLeave('pricing')}
-              >
-                <Link to="/pricing">Company&nbsp; <i className="fas fa-chevron-down"></i></Link>
-                {dropdownOpen.pricing && (
-                  <ul className="dropdown-menu">
-                    <li><Link to="/pricing/standard">Standard Pricing</Link></li>
-                    <li><Link to="/pricing/enterprise">Enterprise Pricing</Link></li>
-                  </ul>
-                )}
-              </li>
-              
-              {/* Hoverable Dropdown for Resources */}
-              <li 
-                className="dropdown" 
-                onMouseEnter={() => handleMouseEnter('resources')} 
-                onMouseLeave={() => handleMouseLeave('resources')}
-              >
-                <Link to="/resources">Resources&nbsp; <i className="fas fa-chevron-down"></i></Link>
-                {dropdownOpen.resources && (
-                  <ul className="dropdown-menu">
-                    <li><Link to="/resources/blog">Blog</Link></li>
-                    <li><Link to="/resources/guides">Guides</Link></li>
-                    <li><Link to="/resources/case-studies">Case Studies</Link></li>
-                  </ul>
-                )}
-              </li>
-              
-            </ul>
-          </div>
-          
-          <div className="cta-buttons">
-            <Link to="/Login" className="btn-valuation1"><i className="fa fa-lock" style={{ color: '#ffc107' }} aria-hidden="true"></i>&nbsp;  Log In</Link>
-            <Link to="/valuation" className="btn-valuation">Get Started &nbsp;<i className="fa fa-arrow-right" style={{ color: 'white' }}></i></Link>
-          </div>
+        {/* Navigation Links */}
+        <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <ul>
+            <li onClick={handleLinkClick}><Link to="/SellersListing">Marketplace</Link></li>
+            <li onClick={handleLinkClick}><Link to="/buyers">For Sellers</Link></li>
+            
+            {/* Hoverable Dropdown for Pricing */}
+            <li 
+              className="dropdown" 
+              onMouseEnter={() => handleMouseEnter('pricing')} 
+              onMouseLeave={() => handleMouseLeave('pricing')}
+            >
+              <Link to="/pricing">Company&nbsp; <i className="fas fa-chevron-down"></i></Link>
+              {dropdownOpen.pricing && (
+                <ul className="dropdown-menu">
+                  <li onClick={handleLinkClick}><Link to="/pricing/standard">Standard Pricing</Link></li>
+                  <li onClick={handleLinkClick}><Link to="/pricing/enterprise">Enterprise Pricing</Link></li>
+                </ul>
+              )}
+            </li>
+            
+            {/* Hoverable Dropdown for Resources */}
+            <li 
+              className="dropdown" 
+              onMouseEnter={() => handleMouseEnter('resources')} 
+              onMouseLeave={() => handleMouseLeave('resources')}
+            >
+              <Link to="/resources">Resources&nbsp; <i className="fas fa-chevron-down"></i></Link>
+              {dropdownOpen.resources && (
+                <ul className="dropdown-menu">
+                  <li onClick={handleLinkClick}><Link to="/resources/blog">Blog</Link></li>
+                  <li onClick={handleLinkClick}><Link to="/resources/guides">Guides</Link></li>
+                  <li onClick={handleLinkClick}><Link to="/resources/case-studies">Case Studies</Link></li>
+                </ul>
+              )}
+            </li>
+          </ul>
+        </div>
 
-          <div className="burger" onClick={toggleMenu}>
-            <span className={menuOpen ? 'active' : ''}></span>
-            <span className={menuOpen ? 'active' : ''}></span>
-            <span className={menuOpen ? 'active' : ''}></span>
-          </div>
-        </nav>
-      </header>
-    </>
+        {/* Call-to-Action Buttons */}
+        <div className="cta-buttons">
+          <Link to="/Login" className="btn-valuation1">
+            <i className="fa fa-lock" style={{ color: '#ffc107' }} aria-hidden="true"></i>&nbsp; Log In
+          </Link>
+          <Link to="/valuation" className="btn-valuation">
+            Get Started &nbsp;<i className="fa fa-arrow-right" style={{ color: 'white' }}></i>
+          </Link>
+        </div>
+
+        {/* Burger Menu */}
+        <div 
+          className={`burger ${menuOpen ? 'active' : ''}`} 
+          onClick={toggleMenu} 
+          aria-label="Toggle menu"
+          aria-expanded={menuOpen}
+          role="button"
+        >
+          <span className={menuOpen ? 'active' : ''}></span>
+          <span className={menuOpen ? 'active' : ''}></span>
+          <span className={menuOpen ? 'active' : ''}></span>
+        </div>
+      </nav>
+    </header>
   );
 }
 
